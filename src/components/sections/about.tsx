52842d1acb2127e036c60e5fae3ed62ad5900874
giovani souza <giovani.souza@insidecode.io>
2025-02-28 17:57:29 -0300

    feat: add update

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -3,6 +3,7 @@ import Section from "@/components/ui/section";
 import SectionTitle from "@/components/ui/section-title";
 import { useTranslations } from "next-intl";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import SVGComponent from "../logo";
 
 export default function About() {
   const t = useTranslations("about-me");
@@ -10,20 +11,23 @@ export default function About() {
   return (
     <Section id="about">
       <SectionTitle>{t("title")}</SectionTitle>
-      <div className="grid md:grid-cols-2 gap-8 items-center">
-        <div className="flex justify-center items-center hexagon-container">
-          <Avatar className="w-[400px] h-[400px]">
-            <AvatarImage src="./logo.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 items-center ">
+        <div className="flex justify-center items-center py-4  ">
+          <SVGComponent className="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px]" />
         </div>
-        <div className="space-y-4">
-          <h3 className="text-2xl font-semibold text-primary">
+        <div className="space-y-3 md:space-y-4 text-center md:text-left">
+          <h3 className="text-xl sm:text-2xl font-semibold text-primary">
             {t("title2")} Giovani
           </h3>
-          <p className="text-foreground/80">{t("description1")}</p>
-          <p className="text-foreground/80">{t("description2")}</p>
-          <p className="text-foreground/80">{t("description3")}</p>
+          <p className="text-sm sm:text-base text-foreground/80">
+            {t("description1")}
+          </p>
+          <p className="text-sm sm:text-base text-foreground/80">
+            {t("description2")}
+          </p>
+          <p className="text-sm sm:text-base text-foreground/80">
+            {t("description3")}
+          </p>
         </div>
       </div>
     </Section>
