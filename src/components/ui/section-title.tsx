0df45a6c339896fd01001f49923ae6d39b6aa189
giovani souza <giovani.souza@insidecode.io>
2025-02-28 10:36:21 -0300

    feat: add file

---
+++ src/components/ui/section-title.tsx
@@ -0,0 +1,27 @@
+import type React from "react";
+interface SectionTitleProps {
+  children: React.ReactNode;
+  highlight?: string;
+}
+
+export default function SectionTitle({
+  children,
+  highlight,
+}: SectionTitleProps) {
+  if (highlight) {
+    const parts = children!.toString().split(highlight);
+    return (
+      <h2 className="text-4xl font-bold mb-12 text-center">
+        {parts[0]}
+        <span className="text-foreground">{highlight}</span>
+        {parts[1]}
+      </h2>
+    );
+  }
+
+  return (
+    <h2 className="text-4xl font-bold mb-12 text-foreground text-center">
+      {children}
+    </h2>
+  );
+}
---
+++ src/i18n/Config.ts
@@ -0,0 +1,53 @@
+export interface Language {
+  code: string;
+  name: string;
+  flag: string;
+  region: string;
+}
+
+export const languages: Language[] = [
+  {
+    code: "en",
+    name: "English",
+    flag: "🇺🇸",
+    region: "United States",
+  },
+  {
+    code: "pt-br",
+    name: "Português",
+    flag: "🇧🇷",
+    region: "Brasil",
+  },
+  {
+    code: "es",
+    name: "Español",
+    flag: "🇪🇸",
+    region: "España",
+  },
+  {
+    code: "fr",
+    name: "Français",
+    flag: "🇫🇷",
+    region: "France",
+  },
+  {
+    code: "de",
+    name: "Deutsch",
+    flag: "🇩🇪",
+    region: "Deutschland",
+  },
+  {
+    code: "ko",
+    name: "한국어",
+    flag: "🇰🇷",
+    region: "Korea",
+  },
+  {
+    code: "ru",
+    name: "Русский",
+    flag: "🇷🇺",
+    region: "Russia",
+  },
+];
+
+export const defaultLocale = "pt";
