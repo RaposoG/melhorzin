320e92aece6e27b038a5960deaf2abf33ef801d2
Brayan Victor <112779905+strilguidev@users.noreply.github.com>
2025-03-07 11:12:11 -0300

    animation.svg teste

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,19 @@ export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                hello word!
+                <h1>
+                    Brayan Victor
+                </h1>
+                <p>Portfólio em construção</p>
+                <Image
+                        src="/animation.svg"
+                        width={1980}
+                        height={1980}
+                        className="w-full  h-full"
+                        alt="Gif"
+                        draggable="false"
+                        quality={100}
+                    />
             </main>
         </div>
   );
