a41ac9e7b95411ea04836f8cf737e87fb9930251
Brayan Victor <112779905+strilguidev@users.noreply.github.com>
2025-03-07 11:19:32 -0300

    Arrumando a page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <h1>
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-blue-950 text-center">
+                <h1 className="text-2xl">
                     Brayan Victor
                 </h1>
-                <p>Portfólio em construção</p>
+                <p className="text-lg">Portfólio em desenvolvimento</p>
                 <Image
                         src="/animation.svg"
                         width={1980}
