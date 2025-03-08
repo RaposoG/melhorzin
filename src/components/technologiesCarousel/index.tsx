c3ed4002482d20ea2a53fd838bd063adbc319179
giovani souza <giovani.souza@insidecode.io>
2025-03-08 17:05:27 -0300

    feat(technologiesCarousel): add responsiveness to carousel components
    - Updated radius calculation for mobile devices.
    - Adjusted dimensions of carousel elements based on screen size.
    - Modified styles for rotating wheel and icons to ensure proper scaling on mobile.
    - Enhanced center circle dimensions and text sizes for better visibility on smaller screens.
    
    These changes improve the user experience by ensuring the technologies carousel is fully responsive, providing a consistent and visually appealing interface across different devices.

--- src/components/technologiesCarousel/index.tsx
+++ src/components/technologiesCarousel/index.tsx
@@ -16,7 +16,7 @@ function TechnologiesCarousel() {
   const spinSpeedRef = useRef<number>(0.5);
   const t = useTranslations("technologiesCarousel.description");
 
-  const radius = 220;
+  const radius = isMobile ? 110 : 220;
   const angleStep = (2 * Math.PI) / icon.length;
 
   useEffect(() => {
@@ -59,20 +59,44 @@ function TechnologiesCarousel() {
 
   return (
     <div className="flex flex-col justify-center items-center p-4">
-      <div className="relative w-[500px] h-[500px]">
+      <div
+        className={`relative ${
+          isMobile ? "w-[250px] h-[250px]" : "w-[500px] h-[500px]"
+        }`}
+      >
         {/* Orbit lines for visual effect (optional) */}
-        <div className="absolute w-[450px] h-[450px] rounded-full left-[25px] top-[25px]" />
-        <div className="absolute w-[350px] h-[350px] rounded-full left-[75px] top-[75px]" />
-        <div className="absolute w-[250px] h-[250px] rounded-full left-[125px] top-[125px]" />
+        <div
+          className={`absolute ${
+            isMobile
+              ? "w-[225px] h-[225px] left-[12.5px] top-[12.5px]"
+              : "w-[450px] h-[450px] left-[25px] top-[25px]"
+          }`}
+        />
+        <div
+          className={`absolute ${
+            isMobile
+              ? "w-[175px] h-[175px] left-[37.5px] top-[37.5px]"
+              : "w-[350px] h-[350px] left-[75px] top-[75px]"
+          }`}
+        />
+        <div
+          className={`absolute ${
+            isMobile
+              ? "w-[125px] h-[125px] left-[62.5px] top-[62.5px]"
+              : "w-[250px] h-[250px] left-[125px] top-[125px]"
+          }`}
+        />
 
         {/* Rotating wheel with icons */}
         <div
-          className="absolute w-[450px] h-[450px] rounded-full  backdrop-blur-sm"
+          className={`absolute ${
+            isMobile ? "w-[225px] h-[225px]" : "w-[450px] h-[450px]"
+          } rounded-full backdrop-blur-sm`}
           style={{
             transform: `rotate(${rotationAngle}deg)`,
             transition: isSpinning ? "none" : "transform 0.3s ease-out",
-            left: "25px",
-            top: "25px",
+            left: isMobile ? "12.5px" : "25px",
+            top: isMobile ? "12.5px" : "25px",
           }}
         >
           {icon.map((icon, index) => {
@@ -84,7 +108,9 @@ function TechnologiesCarousel() {
             return (
               <div
                 key={icon.name}
-                className="absolute w-20 h-20 rounded-full flex items-center justify-center cursor-pointer transform -translate-x-1/2 -translate-y-1/2 shadow-lg transition-all duration-300 hover:scale-110"
+                className={`absolute ${
+                  isMobile ? "w-10 h-10 " : "w-20 h-20"
+                } rounded-full flex items-center justify-center cursor-pointer transform -translate-x-1/2 -translate-y-1/2 shadow-lg transition-all duration-300 hover:scale-110`}
                 style={{
                   left: `calc(50% + ${x}px)`,
                   top: `calc(50% + ${y}px)`,
@@ -119,7 +145,9 @@ function TechnologiesCarousel() {
 
         {/* Center circle with technology information */}
         <div
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-60 h-60 rounded-full z-20 flex items-center justify-center text-center  backdrop-blur-md transition-all duration-300"
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
+            isMobile ? "w-30 h-30" : "w-60 h-60"
+          } rounded-full z-20 flex items-center justify-center text-center backdrop-blur-md transition-all duration-300`}
           style={{
             borderColor: isDarkMode
               ? hoveredItem?.colorDark
@@ -128,18 +156,19 @@ function TechnologiesCarousel() {
         >
           {hoveredItem ? (
             <div className="flex flex-col items-center p-4 max-h-full">
-              <hoveredItem.IconComponent
-                size={30}
-                color={hoveredItem.colorDark}
-                className="mb-3"
-              />
               <h3
-                className="text-2xl font-bold mb-2"
+                className={`${
+                  isMobile ? "text-sm" : "text-2xl"
+                } font-bold mb-2`}
                 style={{ color: hoveredItem.colorDark }}
               >
                 {hoveredItem.name}
               </h3>
-              <p className="text-sm text-foreground line-clamp-4">
+              <p
+                className={`${
+                  isMobile ? "text-xs" : "text-2xl"
+                } text-foreground line-clamp-4`}
+              >
                 {(() => {
                   const key: any = hoveredItem.name
                     .toLowerCase()
@@ -151,7 +180,7 @@ function TechnologiesCarousel() {
             </div>
           ) : (
             <div className="text-center opacity-70">
-              <SVGComponent className="w-[100px] h-[100px] sm:w-[100px] sm:h-[100px]" />
+              <SVGComponent className="w-[50px] h-[50px] sm:w-[100px] sm:h-[100px]" />
             </div>
           )}
         </div>
